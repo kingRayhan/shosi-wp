8b6ed4bfc5c6bd687b6ef61aaa208c4a8d43111e
kingrayhan <rayhan095@gmail.com>
2017-07-08 03:29:56 +0600

    include 404.php , archieve.php

--- 404.php
+++ 404.php
@@ -15,11 +15,11 @@
          <div class="container">
             <div class="row">
                <div class="col-md-12">
-					<div class="error-page-content">
-						<h1 class="error"><?php echo esc_html__( '404', 'wp_shosi' ); ?></h1>
-						<h2 class="error-title"><?php echo esc_html__( 'Page Not Found', 'wp_shosi' ); ?></h2>
-						<span class="go-home"><a href="<?php echo esc_url( get_home_url() ); ?>"><?php echo esc_html__( 'Go Homepage', 'wp_shosi' ); ?></a></span>
-					</div>
+   					<div class="error-page-content">
+   						<h1 class="error"><?php echo esc_html__( '404', 'wp_shosi' ); ?></h1>
+   						<h2 class="error-title"><?php echo esc_html__( 'Page Not Found', 'wp_shosi' ); ?></h2>
+   						<span class="go-home"><a href="<?php echo esc_url( get_home_url() ); ?>"><?php echo esc_html__( 'Go Homepage', 'wp_shosi' ); ?></a></span>
+   					</div>
                </div>
             </div>
          </div>
--- archive.php
+++ archive.php
@@ -6,46 +6,52 @@
  *
  * @package SHOSI_-_Elegant_Blog_Theme
  */
-
 get_header(); ?>
+	  <header class="page-header">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<?php
+							the_archive_title( '<h1 class="page-title">', '</h1>' );
+							the_archive_description( '<div class="archive-description">', '</div>' );
+						?>
+					</div>
+				</div>
+		   </div>
+	  </header><!-- .page-header -->
+      <!--==========================================================-->
+      <!-- Page Content area start -->
+      <!--==========================================================-->
+      <section class="page-content">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-8">
+               <?php
+               if ( have_posts() ) :
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+                  /* Start the Loop */
+                  while ( have_posts() ) : the_post();
 
-		<?php
-		if ( have_posts() ) : ?>
+                     get_template_part( 'template-parts/content', 'loop' );
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+                  endwhile;
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+                  the_posts_navigation();
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+               else :
 
-			endwhile;
+                  get_template_part( 'template-parts/content', 'none' );
 
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+               endif; ?>
+                  <?php echo get_template_part( 'template-parts/pagination' ); ?>
+               </div>
+               <div class="col-md-4">
+                   <?php get_sidebar('right'); ?>    
+               </div>
+            </div>
+         </div>
+      </section>
+      <!--==========================================================-->
+      <!-- Page Content area End -->
+      <!--==========================================================-->
+<?php get_footer(); ?>
