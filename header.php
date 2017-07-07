7f6bc03160beef3287e3c9d05342b9a79bf1828b
kingrayhan <rayhan095@gmail.com>
2017-07-08 02:07:12 +0600

    make mobile navigation dynamic in wp

--- header.php
+++ header.php
@@ -59,49 +59,19 @@
          <!-- Mobile Menu start -->
          <!--==========================================================-->
          <nav class="mobile-menu hidden">
-            <ul>
-               <li>
-                  <a href="index.html">Home</a>
-                  <ul>
-                     <li><a href="index.html">Homepage 1</a></li>
-                     <li><a href="blog2.html">Homepage 2</a></li>
-                     <li><a href="blog3.html">Homepage 3</a></li>
-                  </ul>
-               </li>
-               <li><a href="404.html">404</a></li>
-               <li>
-                  <a href="about1.html">About</a>
-                  <ul>
-                     <li><a href="about1.html">About 1</a></li>
-                     <li><a href="about2.html">About 2</a></li>
-                     <li><a href="about3.html">About 3</a></li>
-                  </ul>
-               </li>
-               <li>
-                  <a href="single1.html">Single</a>
-                  <ul>
-                     <li><a href="single1.html">Single 1</a></li>
-                     <li><a href="single2.html">Single 2</a></li>
-                     <li><a href="single3.html">Single 3</a></li>
-                  </ul>
-               </li>
-               <li>
-                  <a href="video1.html">Video</a>
-                  <ul>
-                     <li><a href="video1.html">Video 1</a></li>
-                     <li><a href="video2.html">Video 2</a></li>
-                     <li><a href="video3.html">Video 3</a></li>
-                  </ul>
-               </li>
-               <li>
-                  <a href="contact1.html">Contact</a>
-                  <ul>
-                     <li><a href="contact1.html">Contact 1</a></li>
-                     <li><a href="contact2.html">Contact 2</a></li>
-                     <li><a href="contact3.html">Contact 3</a></li>
-                  </ul>
-               </li>
-            </ul>
+            <?php    
+            /**
+               * Displays a navigation menu
+               * @param array $args Arguments
+            */
+               $args = array(
+                  'theme_location' => 'menu-1',
+                  'container' => 'ul',
+                  'menu_class' => 'menu_wrapper',
+
+               );
+            
+               wp_nav_menu( $args ); ?>
          </nav><!-- .mobile-menu.hidden -->
          <!--==========================================================-->
          <!-- Mobile Menu end -->
